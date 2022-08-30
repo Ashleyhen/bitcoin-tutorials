af8ecb3e8a8206a90dda80bd5b996541c8aadb82
ashleyhen <Ashleyhen>
2022-08-29 21:20:24 -0400

    init commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use bitcoincore_rpc::{Client, RpcApi};
+use electrum_client::ElectrumApi;
+
+
+fn main() {
+    println!("Hello, world!");
+    let client= Client::new("http://127.0.0.1:18443",
+        bitcoincore_rpc::Auth::UserPass("polaruser".to_owned(),"polarpass".to_owned())).unwrap();
+
+    let electrum =electrum_client::Client::new("ssl://electrum.blockstream.info:60002").unwrap();
+
+    println!("local connection block count: {}", client.get_block_count().unwrap());
+    println!("electrum connection fee: {}", electrum.relay_fee().unwrap());
+}
